e5ff583d37952e4218d66296376758d59455f3fe
Hussinalifawaz <hussainaliaskar123@gmail.com>
2025-11-15 19:11:04 -0500

    the last commit

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -24,54 +24,90 @@ public_users.post("/register", (req, res) => {
 
 
 public_users.get('/', function (req, res) {
-  res.send(JSON.stringify(books, null, 4));
-});
+    let myPromise = new Promise((resolve, reject) => {
+      resolve(books);
+    });
+  
+    myPromise.then((booksList) => {
+      res.send(JSON.stringify(booksList, null, 4));
+    });
+  });
 
 
-public_users.get('/isbn/:isbn', function (req, res) {
-  const isbn = req.params.isbn;
-  if (books[isbn]) {
-    res.send(books[isbn]);
-  } else {
-    res.send("Unable to find book with that ISBN");
-  }
-});
+  public_users.get('/isbn/:isbn', function (req, res) {
+    const isbn = req.params.isbn;
+    let myPromise = new Promise((resolve, reject) => {
+      if (books[isbn]) {
+        resolve(books[isbn]);
+      } else {
+        reject("Book not found with that ISBN");
+      }
+    });
+  
+    myPromise.then((book) => {
+      res.send(book);
+    }).catch((errorMessage) => {
+      res.send(errorMessage);
+    });
+  });
 
 
 public_users.get('/author/:author', function (req, res) {
   const author = req.params.author;
-  let booksArray = [];
-  let allISBNs = Object.keys(books);
-  allISBNs.forEach(isbn => {
-    let currentBook = books[isbn];
-    if (currentBook.author === author) {
-      booksArray.push(currentBook);
-    }
-  });
-  if (booksArray.length > 0) {
-    res.send(booksArray);
-  } else {
-    res.send("Unable to find books by that author");
+  let myPromise = new Promise((resolve, reject) => {
+    let booksArray = [];
+let allISBNs = Object.keys(books);
+
+allISBNs.forEach(isbn => {
+  let currentBook = books[isbn];
+  if (currentBook.author === author) {
+    booksArray.push(currentBook);
   }
+});
+
+
+if (booksArray.length > 0) {
+  resolve(booksArray); 
+} else {
+  reject("No books found for that author"); 
+};
+});
+myPromise.then((booksList) => {
+    res.send(booksList);
+}).catch((errorMessage) => {
+    res.send(errorMessage);
+});
+
+
 });
 
 
 public_users.get('/title/:title', function (req, res) {
-  const title = req.params.title;
-  let booksArray = [];
-  const allISBNs = Object.keys(books);
-  allISBNs.forEach(isbn => {
-    let currentBook = books[isbn];
-    if (currentBook.title === title) {
-      booksArray.push(currentBook);
-    }
+    const title = req.params.title;
+    let myPromise = new Promise((resolve, reject) => {
+      let booksArray = [];
+      let allISBNs = Object.keys(books);
+      
+      allISBNs.forEach(isbn => {
+        let currentBook = books[isbn];
+        if (currentBook.title === title) {
+          booksArray.push(currentBook);
+        }
+      });
+  
+      if (booksArray.length > 0) {
+        resolve(booksArray);
+      } else {
+        reject("No books found with that title");
+      }
+    });
+  
+    myPromise.then((booksList) => {
+      res.send(booksList);
+    }).catch((errorMessage) => {
+      res.send(errorMessage);
+    });
   });
-  if (booksArray.length > 0) {
-    res.send(booksArray);
-  } else {
-    res.send("Unable to find the book with that title");
-  }
-});
 
 
 public_users.get('/review/:isbn', function (req, res) {
